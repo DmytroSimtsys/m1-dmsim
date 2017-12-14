067bf519c1876da47ac9e1ac112d33b4c78f947e
simtzis@yandex.ua <simtzis@yandex.ua>
2017-12-14 15:00:44 +0200

    add module Hello

---
+++ app/code/local/Amasty/Hello/Model/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: димон
+ * Date: 14.12.2017
+ * Time: 11:54
+ */
+class Amasty_Hello_Model_Product
+{
+    public function syaHello(){
+        echo "hello";
+    }
+}
