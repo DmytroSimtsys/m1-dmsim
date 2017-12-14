78979ffbfd91f1d7444e8a854d899cc8d14567b3
simtzis@yandex.ua <simtzis@yandex.ua>
2017-12-14 20:04:20 +0200

    Adding Content Blocks

--- app/code/local/Amasty/Hello/controllers/MessagesController.php
+++ app/code/local/Amasty/Hello/controllers/MessagesController.php
@@ -10,6 +10,7 @@ class Amasty_Hello_MessagesController extends Mage_Core_Controller_Front_Action
 {
     public function goodbyeAction()
     {
-        echo 'Another Goodbye';
+        $this->loadLayout();
+        $this->renderLayout();
     }
 }
