b77eb66364080bde8aac225a5b999354944f0541
Max Voronov <v0id@list.ru>
2018-01-02 22:27:10 +0200

    Fixed subscription module admin url

---
+++ app/code/local/Amasty/Subscription/controllers/Adminhtml/SubscriptionController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: димон
+ * Date: 02.01.2018
+ * Time: 19:56
+ */
+class Amasty_Subscription_Adminhtml_SubscriptionController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        // $this->loadLayout();
+        // $this->renderLayout();
+        echo "Hello Admin";
+    }
+}
